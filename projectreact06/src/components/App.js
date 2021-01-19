a780143099cc160495d68a4f5433e0fc1fcacd55
sa96125 <sa96125@gmail.com>
2021-01-19 23:12:20 +0900

    2021-01-19

---
+++ projectreact06/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Search from './Search';
+
+const App = () => {
+    return (
+        <Search />
+    );
+}
+
+
+export default App;
---
+++ projectreact06/src/components/Search.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import axios from 'axios';
+
+const Search = () => {
+    return
+}
+
+export default Search;
