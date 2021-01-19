06e8bc904e683119a09226cf080616205975cd6a
sa96125 <sa96125@gmail.com>
2021-01-19 23:25:40 +0900

    2021-01-19

--- projectreact06/src/components/Search.js
+++ projectreact06/src/components/Search.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+// 펑셔널 컴포넌트로 이전에 클래스 컴포넌트 검색창을 구현한다.
+// Hook을 이용하여 라이프사이클을 대체한다.
 const Search = () => {
-    return
+    const [term, setTerm] = useState('');
+
+    useEffect(() => {
+        const getData = async () => {
+            const response = await axios.get('', {
+                params: {
+
+                }
+            });
+        }
+
+        if (term) {
+            getData();
+        }
+
+        return () => {
+            //clean up function
+            //settimeout설정.
+        }
+    }, [term]);
+
+    return (
+        <div>
+            <div>
+                <input onChange={e => setTerm(e.target.value)} />
+            </div>
+        </div>
+    );
 }
 
 export default Search;
--- projectreact06/src/index.js
+++ projectreact06/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
     return <App />;
 }
