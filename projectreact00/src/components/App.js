90ccd3934cf9a929a74860c10955ef503a9642d9
sa96125 <sa96125@gmail.com>
2021-02-13 23:38:59 +0900

    2021-02-13

--- projectreact00/src/components/App.js
+++ projectreact00/src/components/App.js
@@ -4,11 +4,14 @@ import youtube from '../apis/youtube';
 
 const App = () => {
 
+  // 채널 아이디 받고, statisticAPI로 그정보로 구독자수, 썸네일, 등등 내 자료랑 비교한다.
   const updateList = async () => {
     const res = await youtube.get('/subscriptions');
     console.log(res);
   }
   updateList();
+
+
 
   return (
     <div>
