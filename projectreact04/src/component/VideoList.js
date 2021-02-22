f9eeca2da1621ad9a777f4cf6df57ee83ed9dd01
sa96125 <sa96125@gmail.com>
2021-02-22 22:15:03 +0900

    2021-02-22

--- projectreact04/src/component/VideoList.js
+++ projectreact04/src/component/VideoList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
+// 요청한 데이터를 정리하는 구간
 const VideoList = ({ videos, onSelectVideo }) => {
     const renderedList = videos.map((video) => {
         return <VideoItem key={video.id.videoId} video={video} onSelectVideo={onSelectVideo} />;
