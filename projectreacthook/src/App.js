5899c3a2302fb32da0b147ee1b0799a600aa1465
sa96125 <sa96125@gmail.com>
2021-09-19 17:22:02 +0900

    create hook useTabs

--- projectreacthook/src/App.js
+++ projectreacthook/src/App.js
@@ -1,13 +1,33 @@
 import "./App.css";
 import { useInput } from "./hooks/useInput";
+import { useTabs } from "./hooks/useTabs";
+
+const data = [
+  {
+    title: "basic react project",
+    content: "learn create-react-app",
+  },
+  {
+    title: "react hooks",
+    content: "awesome functionality",
+  },
+];
 
 const App = () => {
   const maxLen = (value) => value.length <= 10;
   const name = useInput("Mr. ", maxLen);
 
+  const { currentIdx, currentTab } = useTabs("0", data);
+
   return (
     <>
       <input {...name} />
+      <div>
+        {data.map((section, index) => (
+          <button onClick={() => currentIdx(index)}>{section.title}</button>
+        ))}
+        <div>{currentTab.content}</div>
+      </div>
     </>
   );
 };
